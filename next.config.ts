dd0343f64b6519acf7e5520040931df51f2385fe
Kishor Irnak <kishor@example.com>
2026-02-02 23:36:37 +0530

    Configure GitHub Pages deployment with static export and automated workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Change this to your repository name
+  basePath: isProd ? "/Watches-UpFront" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
